7fc4de8a6a5337e9162ca5b7cc5597edae41e302
Saumya41 <saumyashukla4199@gmail.com>
2024-08-07 00:29:01 +0530

    updated answer route

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from config.config import initiate_database
 from routes.admin import router as AdminRouter
 from routes.student import router as StudentRouter
 from routes.question import router as QuestionRouter
+from routes.answer import router as Answerrouter
 
 app = FastAPI()
 
@@ -16,9 +17,10 @@ async def start_database():
 
 @app.get("/", tags=["Root"])
 async def read_root():
-    return {"message": "Welcome to Assessment Platform."}
+    return {"message": "Welcome to Assessment P latform."}
 
 
 app.include_router(AdminRouter, tags=["Administrator"], prefix="/admin")
 app.include_router(QuestionRouter, prefix="/quiz", tags=["quiz"],dependencies=[Depends(token_listener)],)
 app.include_router(StudentRouter,tags=["Students"],prefix="/student",dependencies=[Depends(token_listener)],)
+app.include_router(Answerrouter, prefix="/answers", tags=["answers"])
